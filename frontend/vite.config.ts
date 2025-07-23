fd4ec245861caae48b30d8efbe36510cfd1bb706
Trent Kelly <trentjkelly@outlook.com>
2025-07-22 22:41:37 -0500

    Development environment for frontend edited

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 3000,
+		host: true,
+		strictPort: true,
+		allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0']
+	}
 });
