bcb0f5b73f5909de3377441a5932f6c9cf53e279
Trent Kelly <123403148+trentjkelly@users.noreply.github.com>
2026-06-25 12:52:12 -0500

    SSL Configured back to HTTP for Dev Environment (#32)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig, type PluginOption } from 'vite';
 
 export default defineConfig({
-	plugins: [basicSsl(), sveltekit()] as PluginOption[],
+	plugins: [sveltekit()] as PluginOption[],
 	server: {
 		port: 3000,
 		host: true,
